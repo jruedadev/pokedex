b7dc33c015a50b5d873218762508e522d53659f5
Johan Rueda <librajohan2000@gmail.com>
2023-01-14 14:14:37 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div class="body closed">
+        <div class="open d-none">
+
+        </div>
+        <div class="closed">
+            <div class="status"></div>
+            <div class="red"></div>
+            <div class="blue"></div>
+            <div class="yellow"></div>
+        </div>
+    </div>
+</body>
+
+</html>
